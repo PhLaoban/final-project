d587140faf7f61ec80495b3db59cfa9c53139c49
Philipp Anton <philippanton140396@gmail.com>
2022-07-22 16:49:41 +0200

    Commit changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,27 +1,30 @@
-// playwright.config.ts
-import { devices, type PlaywrightTestConfig } from '@playwright/test';
+import { devices, PlaywrightTestConfig } from '@playwright/test';
 
+// config file reference - https://playwright.dev/docs/test-configuration
 const config: PlaywrightTestConfig = {
-  forbidOnly: !!process.env.CI,
+  webServer: {
+    command: 'yarn start',
+    port: 3000,
+    reuseExistingServer: !process.env.CI,
+  },
+  testIgnore: '**/util/__tests__/**',
   retries: process.env.CI ? 2 : 0,
+  reporter: process.env.CI ? 'list' : 'html',
   use: {
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
   },
-  // ignore JEST test files
-  testIgnore: '**/util/__tests__/**',
+
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+      },
     },
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
   ],
+  outputDir: 'test-results/',
 };
+
 export default config;
